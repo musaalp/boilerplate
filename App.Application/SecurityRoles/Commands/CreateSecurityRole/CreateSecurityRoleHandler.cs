2c25e718155cc40f33040412abf6ff82f8f4a5de
Musa Alp <musaa.alp@gmail.com>
2018-11-23 16:50:59 +0300

    add create security role handler

---
+++ App.Application/SecurityRoles/Commands/CreateSecurityRole/CreateSecurityRoleHandler.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace App.Application.SecurityRoles.Commands.CreateSecurityRole
+{
+    public class CreateSecurityRoleHandler : IRequestHandler<CreateSecurityRoleCommand, int>
+    {
+        public CreateSecurityRoleHandler()
+        {
+            //todo: inject orm context
+        }
+
+        public Task<int> Handle(CreateSecurityRoleCommand request, CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
