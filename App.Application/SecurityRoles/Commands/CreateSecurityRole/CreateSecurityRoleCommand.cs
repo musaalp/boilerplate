97323d25bd45a49b30eda60be4c4a87da8833bc7
Musa Alp <musaa.alp@gmail.com>
2018-11-23 16:50:42 +0300

    add create security role request object

---
+++ App.Application/SecurityRoles/Commands/CreateSecurityRole/CreateSecurityRoleCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace App.Application.SecurityRoles.Commands.CreateSecurityRole
+{
+    public class CreateSecurityRoleCommand : IRequest<int>
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
