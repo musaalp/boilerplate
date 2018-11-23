149939268093a912f8dbdb791114cd8913a40813
Musa Alp <musaa.alp@gmail.com>
2018-11-23 16:39:26 +0300

    add permission query handler

---
+++ App.Application/Permissions/Queries/GetPermissions/GetPermissionsQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace App.Application.Permissions.Queries.GetPermissions
+{
+    public class GetPermissionsQueryHandler : IRequestHandler<GetPermissionsQuery, PermissionsViewModel>
+    {
+        public GetPermissionsQueryHandler()
+        {
+            //todo: inject orm context
+        }
+
+        public Task<PermissionsViewModel> Handle(GetPermissionsQuery request, CancellationToken cancellationToken)
+        {
+            //todo: fetch from db..
+            throw new NotImplementedException();
+        }
+    }
+}
