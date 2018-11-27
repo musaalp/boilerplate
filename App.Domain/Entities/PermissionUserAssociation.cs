120c3f6e72bee4ade0d7fa8ebbe7af521cacc03c
Musa Alp <musaa.alp@gmail.com>
2018-11-27 14:23:09 +0300

    add a new entity permissionuserassociation

---
+++ App.Domain/Entities/PermissionUserAssociation.cs
@@ -0,0 +1,8 @@
+﻿namespace App.Domain.Entities
+{
+    public class PermissionUserAssociation : Entity
+    {
+        public virtual Permission SecurityRole { get; set; }
+        public virtual User User { get; set; }
+    }
+}
