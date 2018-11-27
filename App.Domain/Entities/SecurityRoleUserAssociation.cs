47039ad5d34ac93a917b7259ab1bf2d98dc07b09
Musa Alp <musaa.alp@gmail.com>
2018-11-27 10:45:28 +0300

    add securityroleuserassociation entity

---
+++ App.Domain/Entities/SecurityRoleUserAssociation.cs
@@ -0,0 +1,8 @@
+﻿namespace App.Domain.Entities
+{
+    public class SecurityRoleUserAssociation : Entity
+    {
+        public virtual SecurityRole SecurityRole { get; set; }
+        public virtual User User { get; set; }
+    }
+}
