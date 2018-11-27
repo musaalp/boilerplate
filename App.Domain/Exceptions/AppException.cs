e807deef5af888ea6e0459394a360d50124b2205
Musa Alp <musaa.alp@gmail.com>
2018-11-27 10:46:08 +0300

    add exception objects

---
+++ App.Domain/Exceptions/AppException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace App.Domain.Exceptions
+{
+    public abstract class AppException : Exception
+    {
+        public AppException(Error error, Exception innerException = null)
+            : base(error.Message, innerException)
+        {
+
+        }
+    }
+}
---
+++ App.Domain/Exceptions/SecurityException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace App.Domain.Exceptions
+{
+    public class SecurityException : AppException
+    {
+        public SecurityException(Error error, Exception innerException = null) : base(error, innerException)
+        {
+        }
+    }
+}
