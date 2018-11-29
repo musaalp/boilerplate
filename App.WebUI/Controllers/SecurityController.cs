c5b054eaca7b91bed94641a1f23702625a84d01e
Musa Alp <musaa.alp@gmail.com>
2018-11-29 14:51:29 +0300

    start to build security api controller

---
+++ App.WebUI/Controllers/SecurityController.cs
@@ -0,0 +1,28 @@
+﻿using App.Application.SecurityRoles.Commands.CreateSecurityRole;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace App.WebUI.Controllers
+{
+    //api/security
+    public class SecurityController : BaseController
+    {
+        // post : api/security/create
+        [HttpPost]
+        [Route("create")]
+        public async Task<IActionResult> Create([FromBody] CreateSecurityRoleCommand command)
+        {
+            var securityRoleId = await Mediator.Send(command);
+            return Ok(securityRoleId);
+        }
+
+        // get: api/security/3
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            return Ok(5);
+        }
+    }
+}
