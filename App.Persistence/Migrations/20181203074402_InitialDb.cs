7c925e1b218f653f3afc6fba618c9a91d699e421
Musa Alp <musaa.alp@gmail.com>
2018-12-03 13:39:40 +0300

    generate first migration

---
+++ App.Persistence/Migrations/20181203074402_InitialDb.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace App.Persistence.Migrations
+{
+    public partial class InitialDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Permissions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CreateDate = table.Column<DateTime>(nullable: false),
+                    UpdateDate = table.Column<DateTime>(nullable: false),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    GroupName = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Permissions", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SecurityRoles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CreateDate = table.Column<DateTime>(nullable: false),
+                    UpdateDate = table.Column<DateTime>(nullable: false),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SecurityRoles", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CreateDate = table.Column<DateTime>(nullable: false),
+                    UpdateDate = table.Column<DateTime>(nullable: false),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SecurityRolePermissionAssociations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CreateDate = table.Column<DateTime>(nullable: false),
+                    UpdateDate = table.Column<DateTime>(nullable: false),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    SecurityRoleId = table.Column<int>(nullable: true),
+                    PermissionId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SecurityRolePermissionAssociations", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SecurityRolePermissionAssociations_Permissions_PermissionId",
+                        column: x => x.PermissionId,
+                        principalTable: "Permissions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_SecurityRolePermissionAssociations_SecurityRoles_SecurityRoleId",
+                        column: x => x.SecurityRoleId,
+                        principalTable: "SecurityRoles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PermissionUserAssociations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CreateDate = table.Column<DateTime>(nullable: false),
+                    UpdateDate = table.Column<DateTime>(nullable: false),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    SecurityRoleId = table.Column<int>(nullable: true),
+                    UserId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PermissionUserAssociations", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PermissionUserAssociations_Permissions_SecurityRoleId",
+                        column: x => x.SecurityRoleId,
+                        principalTable: "Permissions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_PermissionUserAssociations_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SecurityRoleUserAssociations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CreateDate = table.Column<DateTime>(nullable: false),
+                    UpdateDate = table.Column<DateTime>(nullable: false),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    SecurityRoleId = table.Column<int>(nullable: true),
+                    UserId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SecurityRoleUserAssociations", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SecurityRoleUserAssociations_SecurityRoles_SecurityRoleId",
+                        column: x => x.SecurityRoleId,
+                        principalTable: "SecurityRoles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_SecurityRoleUserAssociations_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PermissionUserAssociations_SecurityRoleId",
+                table: "PermissionUserAssociations",
+                column: "SecurityRoleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PermissionUserAssociations_UserId",
+                table: "PermissionUserAssociations",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SecurityRolePermissionAssociations_PermissionId",
+                table: "SecurityRolePermissionAssociations",
+                column: "PermissionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SecurityRolePermissionAssociations_SecurityRoleId",
+                table: "SecurityRolePermissionAssociations",
+                column: "SecurityRoleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SecurityRoleUserAssociations_SecurityRoleId",
+                table: "SecurityRoleUserAssociations",
+                column: "SecurityRoleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SecurityRoleUserAssociations_UserId",
+                table: "SecurityRoleUserAssociations",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PermissionUserAssociations");
+
+            migrationBuilder.DropTable(
+                name: "SecurityRolePermissionAssociations");
+
+            migrationBuilder.DropTable(
+                name: "SecurityRoleUserAssociations");
+
+            migrationBuilder.DropTable(
+                name: "Permissions");
+
+            migrationBuilder.DropTable(
+                name: "SecurityRoles");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
