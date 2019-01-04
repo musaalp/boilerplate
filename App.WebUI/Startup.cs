288ed7a601f643ed134ac3ee7d3d594ce8979264
Musa Alp <musaa.alp@gmail.com>
2019-01-04 11:43:15 +0300

    add jwt authentication middleware

--- App.WebUI/Startup.cs
+++ App.WebUI/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using App.Application.Permissions.Queries.GetPermissions;
 using App.Persistence.Contexts;
 using MediatR;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -8,7 +9,11 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System;
 using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace App.WebUI
 {
@@ -29,7 +34,7 @@ namespace App.WebUI
 
             //Add DbContext /MSSQL Server
             services.AddDbContext<AppDbContext>(options =>
-            options.UseSqlServer(Configuration.GetConnectionString("AppConnection")));
+                options.UseSqlServer(Configuration.GetConnectionString("AppConnection")));
 
             services.Configure<CookiePolicyOptions>(options =>
             {
@@ -38,8 +43,42 @@ namespace App.WebUI
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateAudience = true,
+                    ValidAudience = "audience.boilerplate",
+                    ValidateIssuer = true,
+                    ValidIssuer = "issuer.boilerplate",
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ClockSkew = TimeSpan.Zero,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("boilerplateSigningKey"))
+                };
+
+                options.Events = new JwtBearerEvents
+                {
+                    OnTokenValidated = ctx =>
+                    {
+                        System.Console.WriteLine("Token validated");
+                        return Task.CompletedTask;
+                    },
+
+                    OnAuthenticationFailed = ctx =>
+                    {
+                        System.Console.WriteLine($"Exception: {ctx.Exception.Message}");
+                        return Task.CompletedTask;
+                    }
+                };
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -55,6 +94,8 @@ namespace App.WebUI
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.UseAuthentication();
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
