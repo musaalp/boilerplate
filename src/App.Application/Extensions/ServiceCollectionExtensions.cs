c743bb6e36da4a81b1b0bbf0f45e0c547f8ff4c3
Musa Alp <musaa.alp@gmail.com>
2020-04-28 23:01:05 +0300

    feat: Implement MediatR package

---
+++ src/App.Application/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace App.Application.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddApplication(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+        }
+    }
+}
