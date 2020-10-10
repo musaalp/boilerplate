a95ccfde7d8b4a9ad4a675ca3bb1ef1fac8b9c3e
Musa Alp <musaa.alp@gmail.com>
2020-10-10 20:47:53 +0300

    feat: Implement rabbit mq

---
+++ src/App.Infrustructure.RabbitMQ/IChannelFactory.cs
@@ -0,0 +1,14 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace App.Infrustructure.RabbitMQ
+{
+    public interface IChannelFactory : IDisposable
+    {
+        Task<IModel> CreateAsync(CancellationToken cancellationToken = default(CancellationToken));
+    }
+}
---
+++ src/App.Infrustructure.RabbitMQ/RabbitMQBusMessageSubscriber.cs
@@ -0,0 +1,16 @@
+﻿using App.Application.Bus;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Infrustructure.RabbitMQ
+{
+    public class RabbitMQBusMessageSubscriber : IBusMessageSubscriber
+    {
+        public Task SubscribeAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
