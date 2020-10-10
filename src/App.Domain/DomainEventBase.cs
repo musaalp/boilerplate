c1544f83fed162f59fc88d47ebb37c73abdb9695
Musa Alp <musaa.alp@gmail.com>
2020-10-10 20:49:51 +0300

    feat: Add domain event items

---
+++ src/App.Domain/DomainEventBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.Domain
+{
+    public class DomainEventBase : IDomainEvent
+    {
+        public Guid Id { get; protected set; }
+        public DateTime OccurredOn { get; protected set; }
+    }
+}
