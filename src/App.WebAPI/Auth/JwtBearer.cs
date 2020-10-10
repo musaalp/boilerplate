c9b52f15d5e8212df8241d8f120b951ec7b47524
Musa Alp <musaa.alp@gmail.com>
2020-10-10 19:43:58 +0300

    feat: Implement fundemental of auth structure

---
+++ src/App.WebAPI/Auth/JwtBearer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App.WebAPI.Auth
+{
+    public class JwtBearer
+    {
+        public const string SigninKey = "!SigningKey.$1-!";
+    }
+}
