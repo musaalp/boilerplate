ff4ee586eae47874ede771f3edd1e71fa190ac0a
Musa Alp <musaa.alp@gmail.com>
2020-10-10 20:47:53 +0300

    feat: Implement rabbit mq

---
+++ src/App.Infrustructure.RabbitMQ/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace App.Infrustructure.RabbitMQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/App.Infrustructure.RabbitMQ/RabbitMQConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.Infrustructure.RabbitMQ
+{
+    public class RabbitMQConstants
+    {
+        public const string NotificationExchangeName = "Notification_Exchange";
+        public const string NotificationQueueName = "Notification_Queue";
+        public const string NotificationRouteKey = "Notification_RouteKey";
+    }
+}
