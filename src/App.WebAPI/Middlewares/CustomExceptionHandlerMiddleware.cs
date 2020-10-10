a1c59e5604bc28f5ac7dc1770d588c1dd4915b76
Musa Alp <musaa.alp@gmail.com>
2020-10-10 21:44:36 +0300

    feat: Add custom exception middle ware

---
+++ src/App.WebAPI/Middlewares/CustomExceptionHandlerMiddleware.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using App.Application.Exceptions;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace App.WebAPI.Middelewares
+{
+    public class CustomExceptionHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<CustomExceptionHandlerMiddleware> _logger;
+
+        public CustomExceptionHandlerMiddleware(RequestDelegate next, ILogger<CustomExceptionHandlerMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.ToString());
+
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            var code = HttpStatusCode.InternalServerError;
+
+            var result = string.Empty;
+
+            switch (exception)
+            {
+                case EntityNotFoundException entityNotFoundException:
+                    code = HttpStatusCode.NotFound;
+                    result = entityNotFoundException.Message;
+                    break;
+                case ValidationException validationException:
+                    code = HttpStatusCode.BadRequest;
+                    result = JsonConvert.SerializeObject(validationException.Failures);
+                    break;
+            }
+
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int) code;
+
+            if (result == string.Empty)
+            {
+                result = JsonConvert.SerializeObject(new { error = exception.Message });
+            }
+
+            return context.Response.WriteAsync(result);
+        }
+    }
+
+    public static class CustomExceptionHandlerMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseCustomExceptionHandler(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<CustomExceptionHandlerMiddleware>();
+        }
+    }
+}
