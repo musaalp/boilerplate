86fcca5e1064c133e18b283bd7ee73b39e45165e
Musa Alp <musaa.alp@gmail.com>
2020-10-10 21:44:16 +0300

    feat: Add fluent validation with an empty validator

---
+++ src/App.Application/Auths/Queries/LoginUserQueryValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.Application.Auths.Queries
+{
+    public class LoginUserQueryValidator : AbstractValidator<LoginUserQuery>
+    {
+        public LoginUserQueryValidator()
+        {
+            RuleFor(r => r.Username)
+                .NotEmpty()
+                .WithMessage(r => $"{r.Username}_should_not_be_null_or_empty");
+
+            RuleFor(r => r.Password)
+                .NotEmpty()
+                .WithMessage(r => $"{r.Password}_should_not_be_null_or_empty");
+        }        
+    }
+}
